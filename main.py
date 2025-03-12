6a7a3fcfd34973b36914440a5bd07bec0aaa778b
Poornima <poornima.godavarthy@gmail.com>
2025-03-11 17:58:06 -0700

    make reservation progress

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import getpass
 import mysql.connector
+from datetime import datetime
+
 
 def connect_to_database():
-    db_password = getpass.getpass("Enter the database password: ")
-    conn = mysql.connector.connect(user='pgodavar', password=db_password,
+    #db_password = getpass.getpass("Enter the database password: ")
+    conn = mysql.connector.connect(user='pgodavar', password='Wtr25_365_028373715',
                                 host='mysql.labthreesixfive.com',
                                 database='pgodavar')
     return conn
 
 def get_rooms(conn):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM lab7_rooms")
+    cursor.execute("SELECT * FROM lab7_reservations")
     result = cursor.fetchall()
     print(result)
 
 
-def make_reservation(conn, firstname, lastname, roomcode, begindate, enddate, num_children, num_adults):
+def make_reservation(conn, firstname, lastname, roomcode, bedtype, begindate, enddate, num_children, num_adults):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM lab7_rooms")
+    # check room availability with a query
+        # if Any, show *
+    # display available rooms
+        # roomcode, room name, num beds, price
+    # calc total price
+    # insert into lab_7_reservations
+    roomcode, bedtype = str(roomcode), str(bedtype)
+    begindate = datetime.strptime(begindate, "%Y-%m-%d")
+    enddate = datetime.strptime(enddate, "%Y-%m-%d")
+    if roomcode == "Any" and bedtype == "Any":
+        query = f"""
+        SELECT * 
+        FROM lab7_reservations 
+        JOIN lab7_rooms ON lab7_rooms.RoomCode = lab7_reservations.Room
+        WHERE Room = '{roomcode}' AND bedType = '{bedtype}'
+        """
+    else:
+        query = """
+        SELECT RoomCode, RoomName, BedType, maxOcc, basePrice
+        FROM lab7_reservations 
+        JOIN lab7_rooms ON lab7_rooms.RoomCode = lab7_reservations.Room
+        """
+    cursor.execute(query)
     result = cursor.fetchall()
-    print(result)
+    return result
 
 def cancel_reservation(conn):
     pass
@@ -52,11 +76,13 @@ def main():
             firstname = input("FirstName: ")
             lastname = input("LastName: ")
             roomcode = input("Room code or Any: ")
+            bedtype = input("Bed Type: ")
             begindate = input("Begin Date: ")
             enddate = input("End Date: ")
             num_children = input("Number of Children: ")
             num_adults = input("Number of Adults: ")
-            make_reservation(conn, firstname, lastname, roomcode, begindate, enddate, num_children, num_adults)
+            res = make_reservation(conn, firstname, lastname, roomcode, bedtype, begindate, enddate, num_children, num_adults)
+            print(res)
         elif choice == 3:
             cancel_reservation(conn)
         elif choice == 4: 
@@ -69,6 +95,6 @@ def main():
         else:
             print("Invalid choice. Please try again.")
             break
-
+cancel_reservation
 if __name__ == "__main__":
     main()
